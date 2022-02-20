366ce3e9d45e899f79b32d690d75dbced8e4c598
Ryan Uzumaki <ryandx0de@outlook.com>
2022-02-20 10:01:53 +0800

    ...

--- main.cpp
+++ main.cpp
@@ -13,8 +13,7 @@
 
 using namespace std;
 using namespace cv;
-int known_W = 9;
-int known_P = 510;
+
 
 double get_distance(int W, int P);
 void object_recognition(Mat& image);
@@ -40,6 +39,7 @@ int main() {
 	VideoCapture capture_1(0);
 	Mat frame;
 	Mat frame_1;
+	Process object;
 	while (true) {
 		capture.read(frame);
 		waitKey(1);
@@ -47,46 +47,7 @@ int main() {
 		if (frame.empty()||frame_1.empty()) {
 			break;
 		}
-		Mat temp = Mat::zeros(frame.size(), frame.type());
-		Mat m = Mat::zeros(frame.size(), frame.type());
-		addWeighted(frame, 0.19, m, 0.0, 0, temp);
-		Mat dst;
-		bilateralFilter(temp, dst, 5, 20, 20);
-		Mat m_ResImg;
-		cvtColor(dst, m_ResImg, COLOR_BGR2HSV);
-		Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
-		erode(m_ResImg, m_ResImg, element);//进行腐蚀操作
-		erode(m_ResImg, m_ResImg, element);//进行腐蚀操作
-		erode(m_ResImg, m_ResImg, element);//进行腐蚀操作
-		Mat mask;
-		inRange(m_ResImg, Scalar(100, 43, 46), Scalar(124, 255, 255), mask);
-		vector<vector<Point>> contours;
-		vector<Vec4i> hierarchy;
-		findContours(mask, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point());
-		vector<double> area;
-		for (int i = 0; i < contours.size(); i++) {
-			area.push_back(contourArea(contours[i]));
-		}
-		size_t maxIndex = argmax(area.begin(), area.end());
-		Rect ret_1 = boundingRect(contours[maxIndex]);
-		int avgX, avgY;
-		avgX = (ret_1.x + ret_1.width) / 2;//x-axis middle point
-		avgY = (ret_1.y + ret_1.height) / 2;//y-axis middle point
-		for (int i = 0; i < contours.size(); i++) {
-			//for (int j = 0; j < contours[i].size(); j++) {
-			//	Point P = Point(contours[i][j].x, contours[i][j].y);
-			//	Mat Contours = Mat::zeros(m_ResImg.size(), CV_8UC1);  //绘制
-			//	Contours.at<uchar>(P) = 255;
-			//}
-			Rect box(ret_1.x, ret_1.y, ret_1.width, ret_1.height);
-			rectangle(frame, box, Scalar(0, 0, 255), 2, 8, 0);
-			drawContours(frame, contours, maxIndex, Scalar(0, 255, 0), 2, 8, hierarchy);
-		}
-		double dist = get_distance(known_W, ret_1.width);
-		string dist_str = Convert(dist);
-		putText(frame, "Distance:" + dist_str + "cm", Point(50, 50), FONT_HERSHEY_COMPLEX, 1, Scalar(50, 250, 50), 2, 8);
-		namedWindow("detected", WINDOW_FREERATIO);
-		imshow("detected", frame);
+		object.object_recognition(frame);
 		int c = waitKey(1);
 		if (c == 27) { // 退出
 			break;
--- process_functions.cpp
+++ process_functions.cpp
@@ -38,6 +38,47 @@ double Process::get_distance(int W, int P) {
 	D = (W * F) / P;
 	return D;
 }
-void Process::object_recognition(Mat& image) {
-	
+void Process::object_recognition(Mat& frame) {
+	int known_W = 9;
+	int known_P = 510;
+	Mat temp = Mat::zeros(frame.size(), frame.type());
+	Mat m = Mat::zeros(frame.size(), frame.type());
+	addWeighted(frame, 0.19, m, 0.0, 0, temp);
+	Mat dst;
+	bilateralFilter(temp, dst, 5, 20, 20);
+	Mat m_ResImg;
+	cvtColor(dst, m_ResImg, COLOR_BGR2HSV);
+	Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
+	erode(m_ResImg, m_ResImg, element);//进行腐蚀操作
+	erode(m_ResImg, m_ResImg, element);//进行腐蚀操作
+	erode(m_ResImg, m_ResImg, element);//进行腐蚀操作
+	Mat mask;
+	inRange(m_ResImg, Scalar(100, 43, 46), Scalar(124, 255, 255), mask);
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+	findContours(mask, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point());
+	vector<double> area;
+	for (int i = 0; i < contours.size(); i++) {
+		area.push_back(contourArea(contours[i]));
+	}
+	size_t maxIndex = argmax(area.begin(), area.end());
+	Rect ret_1 = boundingRect(contours[maxIndex]);
+	int avgX, avgY;
+	avgX = (ret_1.x + ret_1.width) / 2;//x-axis middle point
+	avgY = (ret_1.y + ret_1.height) / 2;//y-axis middle point
+	for (int i = 0; i < contours.size(); i++) {
+		//for (int j = 0; j < contours[i].size(); j++) {
+		//	Point P = Point(contours[i][j].x, contours[i][j].y);
+		//	Mat Contours = Mat::zeros(m_ResImg.size(), CV_8UC1);  //绘制
+		//	Contours.at<uchar>(P) = 255;
+		//}
+		Rect box(ret_1.x, ret_1.y, ret_1.width, ret_1.height);
+		rectangle(frame, box, Scalar(0, 0, 255), 2, 8, 0);
+		drawContours(frame, contours, maxIndex, Scalar(0, 255, 0), 2, 8, hierarchy);
+	}
+	double dist = get_distance(known_W, ret_1.width);
+	string dist_str = Convert(dist);
+	putText(frame, "Distance:" + dist_str + "cm", Point(50, 50), FONT_HERSHEY_COMPLEX, 1, Scalar(50, 250, 50), 2, 8);
+	namedWindow("detected", WINDOW_FREERATIO);
+	imshow("detected", frame);
 }
